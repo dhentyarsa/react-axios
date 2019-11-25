cb05080d47d3019ca438416a9f64c8c9509ff1c0
dhentyarsa <dhentyarsa@gmail.com>
2019-11-25 16:03:48 +0700

    commit

--- src/Pages/notHome.js
+++ src/Pages/notHome.js
@@ -9,6 +9,7 @@ class notHome extends Component{
                     <center>
                         <h1>
                             Wiggle-wiggle.
+                            
                         </h1>
                         <img src='https://31.media.tumblr.com/a9d6b265d1f45ce6308ee680cfd2049f/tumblr_ne9n48NBbg1tzms7wo1_400.gif' alt='cat-gif' width='50%' height='50%'></img>
                     </center>
